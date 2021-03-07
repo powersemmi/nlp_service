eed5f2b2952e6a9cd9732cb7139caedf6d6e1e8c
Chebotarev Viktor <powersemmi@gmail.com>
2021-03-07 16:28:53 +0300

    Test db and flask conn

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World!"
+
+@app.route('/_test____conn', methods=['GET', 'POST', 'HEAD', 'PUT', 'DELETE'])
+def test_conn():
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        pass
+    elif request.method == "HEAD":
+        pass
+    elif request.method == "PUT":
+        pass
+    elif request.method == "DELETE":
+        pass
+    else:
+        return "OK"
--- tests/test_sql_connect.py
+++ tests/test_sql_connect.py
@@ -1,7 +1,17 @@
 import pytest
-import sqlalchemy
+import requests
+
+from sqlalchemy import create_engine, engine
+from os import environ, path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def test_sql_connection() -> None:
-    conn: sqlalchemy.engine.Engine = sqlalchemy.create_engine(conn_str, *args, **kwargs)
+    conn: engine.Engine = create_engine(environ.get('DATABASE_URL'))
     assert conn.connect()
-        
+
+def test_flask_conn() -> None:
+    prod_host = environ.get('HOST')
+    assert requests.get(prod_host+"/_test____conn").content.decode('utf-8') == "OK"
